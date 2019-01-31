5584ae2147540d5009087b383e5b00131d9b1cac
bmmoore43 <john3784@msu.edu>
2019-01-31 10:22:47 -0500

    updating normilzation script to fix bugs

--- normalization.py
+++ normalization.py
@@ -1,53 +1,67 @@
 #normalization script
 import sys
-inp= open(sys.argv[1], 'r')
-output= open(str(sys.argv[1])+'_norm.txt', 'w')
+import pandas as pd
+import numpy as np
+df = pd.read_csv(sys.argv[1], sep='\t', index_col = 0, header=0)
 
 #normalize values and write
 def normalize_values(lista):
-    gene = lista[0]
+    #gene = lista[0]
     newlist=[]
-    #clust= lista[1]
-    data= lista[1:]
+    data= lista
     datafl=[]
-    for l in data:
-        try: 
-            float(l)
-            datafl.append(l)
-        except(ValueError):
-            datafl.append('NA')
-    mindata= min(datafl)
-    maxdata= max(datafl)
-    if maxdata == 'NA':
-        pass
-    elif mindata == 'NA':
-        pass
-    else:
+    datafl = data[np.logical_not(np.isnan(data))] # remove NA's to get floats and min/max
+    if len(datafl) > 0: #check list is not empty
+        datafl=np.array(datafl, dtype=np.float32) #convert to float in numpy
+        mindata= np.amin(datafl)
+        maxdata= np.amax(datafl)
+        print(mindata, maxdata)
         dem= float(maxdata)-float(mindata)
         if dem != float(0):
-            for i in datafl:
-                if i != 'NA':
-                    norm= (float(i)-float(mindata))/(dem)
-                    newlist.append(str(norm))
-                else:
-                    newlist.append('NA')
-            newstr= '\t'.join(newlist)
-            output.write('%s\t%s\n' % (gene, newstr))
-        else:
-            for i in datafl:
-                newlist.append(str(0))
-            newstr= '\t'.join(newlist)
-            output.write('%s\t%s\n' % (gene, newstr))
+            for i in data: #have to go back to original data because have removed NAs which can be placeholders
+                    if i != np.nan:
+                            norm= (float(i)-float(mindata))/(dem)
+                            newlist.append(str(norm))
+                    else:
+                        newlist.append(np.nan)
+            #print(newlist)
+            return(newlist)
+        else: #replace with NAs if denominator is = 0
+            dem=float(0.00001)
+            for i in data: #have to go back to original data because have removed NAs which can be placeholders
+                    if i != np.nan:
+                            norm= (float(i)-float(mindata))/(dem)
+                            newlist.append(str(norm))
+                    else:
+                        newlist.append(np.nan)
+            #print(newlist)
+            return(newlist)
+    else: #replace with NAs if all NAs
+        for i in data:
+            newlist.append(np.nan)
+        #print(newlist)
+        return(newlist)
 
-#write header
-header =inp.readline().strip().split('\t')
-headerstr= '\t'.join(header)
-output.write(headerstr+'\n')
         
+
+#get NAs in np format 
+df = df.replace("?",np.nan) 
+df = df.replace("NA",np.nan)
+df = df.replace("",np.nan)   
+
+col_list= list(df.columns.values)
+
 #loop through file
-for line in inp:
-    L= line.strip().split('\t')
-    normalize_values(L)
-    
-inp.close()
-output.close()        
+rows_to_norm = df.index.values.tolist()
+cols_to_norm = list(df)
+
+#df[rows_to_norm] = df[rows_to_norm, ].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+#print(df.loc[rows_to_norm,:])
+
+newdf= df.loc[rows_to_norm,:].apply(normalize_values, axis=1, result_type= 'expand')
+
+print(newdf)
+newdf.columns = col_list
+print(newdf)
+
+newdf.to_csv(path_or_buf=str(sys.argv[1])+'_norm.txt', sep="\t", header=True)       
