c5b2927307ee09baee1e41e22d8150d6c229c2ff
Bethany Moore <bmjohns43@gmail.com>
2020-02-18 15:30:33 -0600

    update to from scipy.stats import chi2

--- Similariry_measures/01_spearman_pandas_20180918.py
+++ Similariry_measures/01_spearman_pandas_20180918.py
@@ -11,7 +11,7 @@ import random
 from scipy.stats.stats import pearsonr
 from scipy.stats.stats import pearsonr
 from scipy import stats
-from scipy.stats import chi2.sf
+from scipy.stats import chi2
 
 file = sys.argv[1]
 SAVE = '/mnt/scratch/john3784/SP/Spearman_' + file
