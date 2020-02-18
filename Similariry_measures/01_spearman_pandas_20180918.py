10446d51b7256f2eba4da3ad5972312efe45f27b
Bethany Moore <bmjohns43@gmail.com>
2020-02-18 11:14:43 -0600

    update instructions and remove hard coded dir

--- Similariry_measures/01_spearman_pandas_20180918.py
+++ Similariry_measures/01_spearman_pandas_20180918.py
@@ -1,6 +1,8 @@
 '''
 export PATH=/mnt/home/azodichr/miniconda3/bin:$PATH
 ### should ssh dev-intel16-k80
+### when submitting to queue input the working directory where the file is
+### change output directory to your scratch space
 '''
 import os,sys
 import pandas as pd
@@ -12,9 +14,9 @@ from scipy import stats
 from scipy.stats import chisqprob
 
 file = sys.argv[1]
-SAVE = '/mnt/scratch/peipeiw/Spearman_EC/Spearman_' + file
+SAVE = '/mnt/scratch/john3784/SP/Spearman_' + file
 
-df = pd.read_csv('/mnt/home/peipeiw/Documents/Pathway_prediction/20180827_all_EC_pathway/Expression_for_EC_genes/'+file, sep='\t', index_col = 0, header = 0)  
+df = pd.read_csv(file, sep='\t', index_col = 0, header = 0)  
 pcc = df.T.corr(method='spearman').round(3)
 pcc.to_csv(SAVE, index=True, header=True,sep="\t")
 
